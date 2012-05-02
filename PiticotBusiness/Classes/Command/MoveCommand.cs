b0c8f3bda547006de8c29cadebfdfa853f74ed9e
uaandrei <ua.andrei@gmail.com>
2012-05-02 22:40:15 +0300

    command design patter

---
+++ PiticotBusiness/Classes/Command/MoveCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PiticotBusiness.Classes.Command
+{
+    public class MoveCommand : UndoableCommand
+    {
+        private Game game;
+        private int steps;
+        private int previousPosition;
+        private Player previousPlayer;
+
+        public MoveCommand(Game game, int steps)
+        {
+            this.game = game;
+            this.steps = steps;
+            previousPosition = game.CurrentPlayer.CurrentCell.Number;
+            previousPlayer = game.CurrentPlayer;
+        }
+
+        public override void Undo()
+        {
+            
+        }
+
+        public override void Execute()
+        {
+
+        }
+    }
+}
