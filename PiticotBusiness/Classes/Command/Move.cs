c605bccc13c66e2929ee8438a10c905834cd7086
uaandrei <ua.andrei@gmail.com>
2012-05-02 20:14:17 +0300

    interface Action and Move class

---
+++ PiticotBusiness/Classes/Command/Move.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PiticotBusiness.Classes.Command
+{
+    internal class Move:Action
+    {
+        public void execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
